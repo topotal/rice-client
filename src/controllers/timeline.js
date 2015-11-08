663290d1295ce492c8fe8f76cdd0cb43bc48048b
sawada <sawadasuiren@gmail.com>
2015-11-08 21:36:41 +0900

    炊飯ボタンがスクロールで消える機能を仮追加

--- src/controllers/timeline.js
+++ src/controllers/timeline.js
@@ -1,3 +1,19 @@
+var hideButtonAnimate = Ti.UI.createAnimation();
+hideButtonAnimate.bottom = -60;
+hideButtonAnimate.duration = 300;
+hideButtonAnimate.curve = Ti.UI.ANIMATION_CURVE_EASE_OUT;
+var isScrolling = false;
+
+var onScroll = (e) => {
+  if(isScrolling) {
+    return;
+  }
+
+  isScrolling = true;
+  $.cookButton.animate(hideButtonAnimate);
+};
+$.table.getView().addEventListener('scroll', onScroll);
+
 var data = [
   {
     name: 'sawa-zen',
@@ -15,43 +31,6 @@ var data = [
     image: 'https://www.mrso.jp/mrsocmscustome/wp-content/uploads/2013/10/%E3%81%8A%E7%B1%B3.jpg'
   }
 ];
-
-//// レコードの保存
-//var record = Alloy.createModel('record', {
-//  name: 'nari',
-//  icon: 'kkkkkkkkkkk',
-//  star: 0
-//});
-//record.save();
-
-
-//// レコードの取得
-//var records = Alloy.Collections.instance('record');
-//records.fetch();
-//records.map((v) => {
-//  console.info(v.get('name'));
-//});
-//// 引数なしで全件取得
-//records.fetch();
-//records.map(function(user){
-//    // レコードの値を取得。
-//    Ti.API.info(user.id);   // IDは自動で付与されるみたい。
-//    Ti.API.info(user.get('name'));
-//    // JSONに変換も簡単。
-//    Ti.API.info(user.toJSON());
-//});
-
-
-//records.fetch({
-//  success: function(collection) {
-//    Ti.API.info('Instruments Collection Retrieved.');
-//    console.info(JSON.stringify(collection));
-//  },
-//  error: function(e) {
-//    alert("Error: " + e);
-//  }
-//});
-
 
 var rows = [];
 _.each(data, (rowData) => {
