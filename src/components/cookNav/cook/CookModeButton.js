89c2315ca57247d23054ff62ee99c51854562e3f
sawada <sawadasuiren@gmail.com>
2016-05-11 12:47:36 +0900

    CookModeButtonクラス追加

---
+++ src/components/cookNav/cook/CookModeButton.js
@@ -0,0 +1,26 @@
+import ColorButton from '../../common/ColorButton';
+
+/**
+ * 炊飯モードボタンクラスです。
+ */
+export default class CookModeButton extends ColorButton {
+
+  /**
+   * モード
+   */
+  get mode() {
+    return this._mode;
+  }
+
+  /**
+   * コンストラクター
+   * @constructor
+   * @param mode
+   */
+  constructor(mode) {
+    super(mode.color, mode.buttonText);
+
+    this._mode = mode;
+  }
+
+}
