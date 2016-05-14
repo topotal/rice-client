acc356289d50ea86b85ad9540a904a97bb5978be
sawada <sawadasuiren@gmail.com>
2016-05-15 00:00:40 +0900

    SelectTableのクリックを修正

--- src/components/form/SelectTable.js
+++ src/components/form/SelectTable.js
@@ -107,7 +107,7 @@ export default class SelectTable extends TiTableView {
    */
   _createRow(index, data) {
     var row = new SelectTableRow(index, data);
-    row.addEventListener('click', () => this._onClickRow({
+    row.addEventListener('wclick', () => this._onClickRow({
       data: data
     }));
 
