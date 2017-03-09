67a21d0ecf1fcaba2002e36d1d5d608d22de227b
sawada <sawadasuiren@gmail.com>
2017-03-10 02:03:06 +0900

    Add react files.

--- src/enum/ApiPath.js
+++
@@ -1,6 +0,0 @@
-export default {
-  COOK_RECORDS: 'http://sawa-zen.com/cook_records.json',
-  BRANDS: 'http://sawa-zen.com/brands.json',
-  QUANTITY: 'http://sawa-zen.com/quantity.json',
-  WATER: 'http://sawa-zen.com/water.json',
-};
