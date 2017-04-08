89caa3b403e5c9c4e5e48cc6ace4abaadb972e51
sawada <sawadasuiren@gmail.com>
2017-04-09 03:15:04 +0900

    Change button to ColorButton.

--- app/views/home/HomeScene.js
+++ app/views/home/HomeScene.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import { View, Text, TouchableHighlight, StyleSheet } from 'react-native';
 import Window from '../common/Window';
 import SceneManager from '../../utils/SceneManager';
-import Button from '../common/Button';
+import ColorButton from '../common/ColorButton';
 import RecipeScene from '../recipe/RecipeScene';
 import CookScene from '../cook/CookScene';
 
@@ -27,13 +27,14 @@ export default class HomeScene extends Component {
    */
   render() {
     return (
-      <Window title="Home">
-        <Button onPress={this._onPressRecipe}>
-          <Text>レシピ</Text>
-        </Button>
-        <Button onPress={this._onPressCook}>
-          <Text>炊飯</Text>
-        </Button>
+      <Window title="Home" style={styles.wrapper}>
+        <ColorButton
+          style={styles.recipeButton}
+          text="レシピ詳細"
+          onPress={this._onPressRecipe} />
+        <ColorButton
+          text="炊飯"
+          onPress={this._onPressCook} />
       </Window>
     );
   }
@@ -54,3 +55,13 @@ export default class HomeScene extends Component {
     SceneManager.instance.to(CookScene);
   }
 }
+
+let styles = {
+  wrapper: {
+    padding: 10
+  },
+  recipeButton: {
+    marginBottom: 10,
+    backgroundColor: '#FF9B00'
+  }
+};
