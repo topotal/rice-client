950306dd601783511dd02f75fdfaac60ec3487e8
sawada <sawadasuiren@gmail.com>
2017-04-08 20:44:51 +0900

    Fix RecipeScene to use Window.

--- app/views/recipe/RecipeScene.js
+++ app/views/recipe/RecipeScene.js
@@ -1,8 +1,4 @@
 import React, { Component } from 'react';
-import { View, Text, TouchableHighlight } from 'react-native';
-import Navbar from '../common/Navbar';
-import Button from '../common/Button';
-import SceneManager from '../../utils/SceneManager';
 
 /**
  * レシピ詳細クラス
@@ -15,8 +11,6 @@ export default class RecipeScene extends Component {
    */
   constructor(props) {
     super(props);
-
-    this._onPressBack = this._onPressBack.bind(this);
   }
 
   /**
@@ -24,34 +18,8 @@ export default class RecipeScene extends Component {
    */
   render() {
     return (
-      <View style={styles.container}>
-        <Navbar title="レシピ">
-          <Button style={styles.backButton} onPress={this._onPressBack}>
-            <Text>戻る</Text>
-          </Button>
-        </Navbar>
-      </View>
+      <Window title="レシピ詳細">
+      </Window>
     );
   }
-
-  /**
-   * 戻るボタン押下時の
-   */
-  _onPressBack() {
-    SceneManager.instance.back();
-  }
 }
-
-const styles = {
-  container: {
-    flex: 1,
-    position: 'relative'
-  },
-  backButton: {
-    position: 'absolute',
-    top: 0,
-    left: 10,
-    bottom: 0,
-    backgroundColor: "#F00"
-  }
-};
