c3d151abb7d929c5905e239c86ef58c5bfc43f55
sawada <sawadasuiren@gmail.com>
2017-04-17 12:18:10 +0900

    Add RecipeWindowStyle.

---
+++ app/styles/mainNav/recipe/RecipeWindowStyle.js
@@ -0,0 +1,10 @@
+import { StyleSheet } from 'react-native';
+
+/**
+ * レシピ画面のスタイルです。
+ */
+export default StyleSheet.create({
+  wrapper: {
+    padding: 10
+  }
+});
--- app/views/mainNav/recipe/RecipeWindow.js
+++ app/views/mainNav/recipe/RecipeWindow.js
@@ -1,10 +1,18 @@
 import React, { Component } from 'react';
+import { View, Text } from 'react-native';
 import Window from '../../common/Window';
+import styles from '../../../styles/mainNav/recipe/RecipeWindowStyle';
+import Const from '../../../styles/Const';
 
 /**
  * レシピ詳細ウィンドウクラス
  */
 export default class RecipeWindow extends Window {
+
+  /** タイトル */
+  _title = 'レシピ詳細';
+  /** navbarの色 */
+  _navbarColor = Const.GREEN;
 
   /**
    * コンストラクター
@@ -12,5 +20,17 @@ export default class RecipeWindow extends Window {
    */
   constructor(props) {
     super(props);
+  }
+
+  /**
+   * コンテンツを描画します。
+   * @override
+   */
+  _renderContent() {
+    return (
+      <View style={styles.wrapper}>
+        <Text style={styles.text}>レシピ詳細が入ります。</Text>
+      </View>
+    );
   }
 }
