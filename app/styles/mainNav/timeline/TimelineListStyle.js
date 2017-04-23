cff8b08cf5551cbd6c52df6fc2f0cd7df59150cf
sawada <sawadasuiren@gmail.com>
2017-04-23 19:45:41 +0900

    Add TimelineListStyle.js

---
+++ app/styles/mainNav/timeline/TimelineListStyle.js
@@ -0,0 +1,15 @@
+import { StyleSheet, Dimensions } from 'react-native';
+import Const from '../../Const';
+
+/**
+ * タイムラインのスタイルです。
+ */
+export default StyleSheet.create({
+  backgroundImage: {
+    flex: 1,
+    width: null,
+    height: null,
+    resizeMode: 'repeat'
+  }
+});
+
