76b24189e47bca0104f0885c7d13d31d967cb4b6
sawada <sawadasuiren@gmail.com>
2017-05-12 23:21:28 +0900

    Add TabBar.js

---
+++ app/views/mainNav/TabBar.js
@@ -0,0 +1,26 @@
+import React, {Component} from 'react';
+import { View, StyleSheet } from 'react-native';
+
+/**
+ * タブバークラスです。
+ */
+export default class TabBar extends Component {
+
+  /**
+   * コンストラクター
+   * @constructor
+   */
+  constructor(props) {
+    super(props);
+  }
+
+  /**
+   * 描画します。
+   */
+  render() {
+    return (
+      <View>
+      </View>
+    );
+  }
+}
