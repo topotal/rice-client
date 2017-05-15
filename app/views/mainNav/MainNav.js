994037830e06b8147fc263e1181621359dc11fe1
sawada <sawadasuiren@gmail.com>
2017-05-15 23:55:48 +0900

    Fix MainNav.

--- app/views/mainNav/MainNav.js
+++ app/views/mainNav/MainNav.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { Navigator, StyleSheet } from 'react-native';
 import NavigationWindow from '../common/NavigationWindow';
-import TimelineWindow from './timeline/TimelineWindow';
+import TabWindow from './tabWindow/TabWindow';
 
 /**
  * メインナビゲーションウィンドウクラスです。
@@ -16,7 +16,7 @@ export default class MainNav extends Component {
     super(props);
 
     this._initialRoute = {
-      component: TimelineWindow
+      component: TabWindow
     }
   }
 
